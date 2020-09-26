5df2b2132044dd453d0bd351dbfb6823b9a19de5
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-09-26 12:50:52 -0700

    change dedupe to const

--- tags.js
+++ tags.js
@@ -2,9 +2,9 @@ const masterHashTags1 = ["ableton", "afrohouse", "beats", "beatport", "breakbeat
 const masterHashTags2 = ["ableton", "beat", "beats", "boombapmusic", "dj", "freeverse", "flgang", "flstudio", "hiphop", "hiphopartist", "hiphopbeats", "hiphopculture", "hiphopdaily", "hiphopdance", "hiphophead", "hiphopheads", "hiphopinstrumentals", "hiphopinternational", "hiphopislife", "hiphopjunkie", "hiphoplife", "hiphoplyrics", "hiphopmusic", "hiphopnation", "hiphopnews", "hiphopparty", "hiphopproducer", "hiphopunderground", "lofihiphop", "logicprox", "lookingforbeats", "makingabeat", "mixtape", "mpc", "mpcstudio", "musicproducer", "newhiphop", "nomumblerap", "producer", "rap", "rapinstrumentals", "rapmusic", "rapper", "rappers", "rapradar", "realrap", "rhymes", "rnb", "rnbbeats", "rnbhiphop", "rnbmusic", "rnbsinger", "soundcloudrapper", "studio", "trap", "trapbeats", "trapbeatsforsale", "trapmusic", "unsignedrapper", "upcomingrapper", "westcoasthiphop"];
 const masterHashTags3 = ["albumoftheday", "ampeg", "artist", "band", "bandmembers", "bandmerch", "bandshirt", "bassist", "blues", "bluesmusic", "concert", "cover", "deathmetal", "drums", "emo", "epiphone", "ernieball", "fender", "gibson", "gothicrock", "grungemusic", "guitar", "guitaristsofinstagram", "guitarmusic", "hardrockband", "heavymetal", "horrorpunk", "ibanezguitars", "indiepop", "indierock", "instaband", "lickwars", "live", "livemusic", "marshallamps", "metal", "metalhead", "music", "musician", "pop", "postgrunge", "poppunk", "postpunk", "powermetal", "prog", "progressiverock", "progrock", "punk", "punkrock", "punks", "riffwars", "rock", "rockband", "rockmusic", "rocknroll", "rockshow", "softgrunge", "stonerrock", "stratocaster", "telecaster", "thrashmetal"];
 
-let masterHashTags = masterHashTags1.concat(masterHashTags2.concat(masterHashTags3));
+const masterHashTags = masterashTags1.concat(masterHashTags2.concat(masterHashTags3));
 
-let dedupe = (arr) => arr.filter((v,i) => arr.indexOf(v) === i);
+const dedupe = (arr) => arr.filter((v,i) => arr.indexOf(v) === i);
 masterHashTags = dedupe(masterHashTags);
 
 module.exports.masterHashTags = masterHashTags;
