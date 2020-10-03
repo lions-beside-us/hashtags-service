58496072c726a71ae79c72aab9836663502fbae5
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-03 13:38:22 -0700

    setup webpack, babel and react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+        <div>Hashtags will be here.. eventually</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#hashtags"));
