7c10dd6dd593484221c60e9ca856bd009a58082c
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-06 20:13:21 -0700

    show hashtags in react with some bootstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div>
-        <div>Hashtags will be here.. eventually</div>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hashtags: []
+    }
+    this.getHashtags = this.getHashtags.bind(this);
+  }
+
+  getHashtags() {
+    // console.log('hi');
+    axios.get('http://localhost:4001/hashtags/13')
+      .then(response => {
+        const hashtags = response.data.data;
+        this.setState({
+          hashtags
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.getHashtags();
+  }
+
+  render() {
+    return (
+      <div className="container">
+          {
+            this.state.hashtags.map(hashtag =>
+                <span className="badge badge-pill badge-secondary mr-1">#{hashtag}</span>
+            )
+          }
+      </div>
+    );
+  }
+
 };
 
 ReactDOM.render(<App />, document.querySelector("#hashtags"));
