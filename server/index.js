c8b4721b06504af124d57fa85c84dc47881997e6
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-10-15 20:20:04 -0700

    add dynamic rendering based on url and updated readme

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const db = require('../database/index');
+const path = require('path');
 
 const port = 4001;
 
 const app = express();
+app.use(express.static('./dist'));
 app.use(express.json());
 app.use(cors());
 
@@ -53,6 +55,11 @@ app.get('/hashtags/:id', async(req, res) => {
       error: error
     });
   }
+});
+
+app.get('/:current', (req, res) => {
+  console.log('hit');
+  res.sendFile(path.join(__dirname,'../dist/index.html'));
 });
 
 app.listen(port, () => {
